4717155a5f1949a99d663f259f2df9d34d101ea7
Josefina Duarte <joseduarte@itba.edu.ar>
2023-07-07 19:08:35 -0300

    bolu

--- Clase_Router.py
+++ Clase_Router.py
@@ -8,22 +8,19 @@ class Router():
     def __str__(self):
         pass
 
-    def cambiar_estado(self):           ##es el mismo para activar, inactivar (agregar se hace cin lista enlazada)
-        pass
-
     def reset(self):
         pass
 
     def analizar_destino_paquete(self):         #El router analiza si el msj tiene como destino ese mismo router (si no lo es se va a transmitir)
         pass
-    
+
     def transmitir_paquete(self):          #Es un metodo del router o de la lista?
         pass
 
     def guardar_datospaquete(self):     #guardar los msjs de los paquetes que pasaron por el router
         pass
 
-    def guardar_datoscambioestado(self):
+    def cambiar_guardar_datoscambioestado(self):             ##es el mismo para activar, inactivar (agregar se hace cin lista enlazada)
         pass
 
     def cantidad_paquetes_envorec(self):        #para tener el dato de la cantidad de paquetes que recibio y envio para el grafico
